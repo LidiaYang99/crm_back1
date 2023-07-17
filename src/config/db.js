a7a97c817d1d3664157a5d7705c777722efd851a
SIgnacioRoccuzzo <129063696+SIgnacioRoccuzzo@users.noreply.github.com>
2023-07-17 18:23:46 +0200

    cambio de usuario

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: '123456',
+    password: 'loboLA22@',
     port: 3306,
     database: 'mydb'
 });
