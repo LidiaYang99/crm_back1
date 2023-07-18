24ada862ddfa37bdb30cc61cfcccff4095b1b0ac
Lidia <lidiayang104@gmail.com>
2023-07-18 12:22:30 +0200

    fin de backend

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: 'loboLA22@',
+    password: '123456',
     port: 3306,
     database: 'mydb'
 });
