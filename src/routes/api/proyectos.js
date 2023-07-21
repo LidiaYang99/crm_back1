553a102da12d8590e8b18ced5e3a541e86c37202
vanessa <vanessaduquerubio@gmail.com>
2023-07-21 13:53:35 +0200

    cambios bd

--- src/routes/api/proyectos.js
+++ src/routes/api/proyectos.js
@@ -1,8 +1,9 @@
-const { getAllProyectos, postNewProyecto, actualizaProyecto, removeProyecto } = require('../../controllers/proyectos.controller');
+const { getAllProyectos, postNewProyecto, actualizaProyecto, removeProyecto, getForMonth } = require('../../controllers/proyectos.controller');
 
 const router = require('express').Router();
 
 router.get('/', getAllProyectos)
+router.get('/:usuarioId/:proyectoId/:mes', getForMonth)
 router.post('/', postNewProyecto)
 router.put('/:proyectoId', actualizaProyecto)
 router.delete('/:proyectoId', removeProyecto);
