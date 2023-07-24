2f6284960e8b6b28f119d5cea7fc43f997d1c123
Lidia <lidiayang104@gmail.com>
2023-07-24 20:14:28 +0200

    correccion para poder crear user nuevo

--- src/routes/api/usuarios.js
+++ src/routes/api/usuarios.js
@@ -10,13 +10,13 @@ router.get('/:usuarioId', /* checkUsuarioId, */ usuariosController.getUser);
 router.get('/:usuarioId/:fecha', checkTokenAdmin, usuariosController.getByDate);
 
 
-router.post('/', checkTokenAdmin, usuariosController.createUsers);
+router.post('/', usuariosController.createUsers);
 router.post('/login/user', usuariosController.checkLoginUser);
-router.post('/horas', checkTokenAdmin, usuariosController.registroHours);
+router.post('/horas', usuariosController.registroHours);
 router.post('/horasdedicadas', usuariosController.horasDedicadas)
 
 
-router.put('/editar/:usuarioId', /*checkTokenAdmin,*/ usuariosController.updateUsuario);
+router.put('/editar/:usuarioId', usuariosController.updateUsuario);
 
 router.delete('/:usuarioId', usuariosController.deleteUsers);
 
