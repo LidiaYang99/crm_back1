3fecc44833d4c9e886b868e34515a682583e8257
Lidia <lidiayang104@gmail.com>
2023-07-27 11:47:27 +0200

    cambios

--- src/routes/api/usuarios.js
+++ src/routes/api/usuarios.js
@@ -17,7 +17,6 @@ router.post('/horasdedicadas', usuariosController.horasDedicadas)
 router.post('/getTime', usuariosController.getHhor)
 router.post('/profile/horasdedicadas', checkTokenUser, usuariosController.horasDedicadas)
 router.put('/editar/:usuarioId', usuariosController.updateUsuario);
-router.put('/editar/user/:usuarioId', usuariosController.updateUserUsuario);
 router.delete('/:usuarioId', usuariosController.deleteUsers);
 
 module.exports = router;
